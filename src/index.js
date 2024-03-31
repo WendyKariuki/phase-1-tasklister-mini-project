4cda630a498fd35f1afdef5a85cd5c59094b4934
WendyKariuki <phaminuswendy@gmail.com>
2024-03-31 21:22:01 +0300

    mini-project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    const taskInput = document.getElementById('new-task-description');
+    const taskDescription = taskInput.value.trim();
+
+    if (taskDescription !== '') {
+      addTask(taskDescription);
+      taskInput.value = ''; 
+    }
+  });
+
+  function addTask(description) {
+    const taskItem = document.createElement('li');
+    taskItem.textContent = description;
+
+    taskList.appendChild(taskItem);
+  }
 });
